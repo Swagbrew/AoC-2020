03e0eddcb33aa4b55431380a0cf87de0e74e6de5
Swagbrew <patryk.binkowski21@gmail.com>
2020-12-08 22:20:04 +0100

    Revert "task1"
    This reverts commit d9ed363c0b5715e94760640e97e6c7ba31bc18a4.

--- task1.py
+++
@@ -1,9 +0,0 @@
-inputs = []
-with open('task1.txt', 'r') as f:
-    for x in f:
-        inputs.append(int(x))
-
-for x in inputs:
-    for y in inputs:
-        if x != y and x+y == 2020:
-            print(x*y)
